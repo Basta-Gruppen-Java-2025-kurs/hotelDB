d939c08aefe2d059ef23bfda3f72ea7d7b463a33
Dimage Sapelkin <kerabromsmu@users.noreply.github.com>
2025-10-30 17:25:27 +0100

    + Added a database dropping option to test data

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -10,10 +10,9 @@ public class MainMenu implements IMenu {
         MenuBuilder mainMenu = new MenuBuilder("Welcome to \033[1mHotel Database\033[22m application.", "Exit");
         mainMenu.addItem("Customer menu", new CustomerControl()::menu)
                 .addItem("Room menu", new RoomControl()::menu)
-                .addItem("Booking menu", new BookingControl()::menu);
-        if (!CreateTestData.testDataExists()) {
-            mainMenu.addItem("Create test data", CreateTestData::createDataIfNotExists);
-        }
+                .addItem("Booking menu", new BookingControl()::menu)
+                .addItem("Create test data", CreateTestData::createDataIfNotExists, () -> !CreateTestData.testDataExists())
+                .addItem("Drop all tables", CreateTestData::dropDatabase, CreateTestData::testDataExists);
         mainMenu.runMenu();
         System.out.println("Goodbye.");
     }
